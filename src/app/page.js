f028795d3b0cbf8c36fb58813589ad515af99e07
Nguyen Trong Nghia (FPL CT) <nghiant96@fpt.edu.vn>
2025-05-25 14:30:53 +0700

    impl new project

---
+++ src/app/page.js
@@ -0,0 +1,157 @@
+"use client"
+import Header from "@/components/Header";
+import { useLottie } from "lottie-react";
+import * as workflowNextStep from "@/assets/workflow-next-step.json";
+
+
+export default function Home() {
+
+  const { View: View_1 } = useLottie({
+    animationData: JSON.parse(JSON.stringify(workflowNextStep)),
+    loop: true,
+    autoplay: true,
+    style: {
+      height: '100%',
+      width: '100%'
+    }
+  });
+  const { View: View_2 } = useLottie({
+    animationData: JSON.parse(JSON.stringify(workflowNextStep)),
+    loop: true,
+    autoplay: true,
+    style: {
+      height: '100%',
+      width: '100%'
+    }
+  });
+  const { View: View_3 } = useLottie({
+    animationData: JSON.parse(JSON.stringify(workflowNextStep)),
+    loop: true,
+    autoplay: true,
+    style: {
+      height: '100%',
+      width: '100%'
+    }
+  });
+  const { View: View_4 } = useLottie({
+    animationData: JSON.parse(JSON.stringify(workflowNextStep)),
+    loop: true,
+    autoplay: true,
+    style: {
+      height: '100%',
+      width: '100%'
+    }
+  });
+  const { View: View_5 } = useLottie({
+    animationData: JSON.parse(JSON.stringify(workflowNextStep)),
+    loop: true,
+    autoplay: true,
+    style: {
+      height: '100%',
+      width: '100%'
+    }
+  });
+
+  return (
+    <div className="container-fluid">
+      <Header />
+        {/* Hero section */}
+      <section className="d-flex container pt-3 pb-3 align-items-center flex-wrap justify-content-center">
+        <div className="col-sm-12 col-lg-6 mb-lg-3 mb-xl-0">
+          <div className="d-flex bg-primary text-white rounded-circle mb-3 justify-content-center align-items-center icon">
+            <i className="bi bi-buildings-fill"></i>
+          </div>
+          <p className="text-primary fw-bold">SmartCode Solutions</p>
+          <h2 className="text-primary">Giải pháp công nghệ thông minh - SmartCode Solutions</h2>
+          <p className="text-content mb-3">
+            SmartCode Solutions mang đến giải pháp thiết kế website, ứng dụng và phần mềm chuyên nghiệp, tối ưu SEO và trải nghiệm người dùng. Hãy bắt đầu hành trình số hóa doanh nghiệp của bạn!
+          </p>
+          <button className="btn btn-primary">Liên hệ ngay</button>
+        </div>
+        <div className="col-6 mobile-hidden ps-3">
+          <img src="/images/overview.svg" alt="Hero Image" className="img-fluid" />
+        </div>
+      </section>
+
+      <section className="container-fluid bg-primary-light pt-3 pb-3">
+        <div className="d-flex flex-column container mb-3 align-items-center flex-wrap justify-content-center">
+          <h2>Quy trình làm việc</h2>
+          <p className="text-content col-sm-12 col-lg-6 text-center">Quy trình thiết kế website, ứng dụng và phần mềm của SmartCode Solutions được xây dựng bài bản và chuyên nghiệp, từ việc tìm hiểu yêu cầu khách hàng đến triển khai và bảo trì.</p>
+        </div>
+        <div className="d-flex container flex-wrap justify-content-center">
+          <div className="col-1">
+            <div className="bg-white rounded-circle mb-3 d-flex justify-content-center align-items-center p-3 position-relative">
+              <img src="/images/workflow-step-01.png" className="img-fluid w-100 h-100"/>
+              <div className="d-flex bg-primary rounded-circle justify-content-center align-items-center badge mobile-hidden">
+                <span className="text">1</span>
+              </div>
+            </div>
+            <div>
+              <p className="text-content text-center">Tư vấn và phân tích yêu cầu</p>
+            </div>
+          </div>
+          <div className="col-1 mobile-hidden">{View_1}</div>
+          <div className="col-1">
+            <div className="bg-white rounded-circle mb-3 d-flex justify-content-center align-items-center p-3 position-relative">
+              <img src="/images/workflow-step-02.png" className="img-fluid w-100 h-100"/>
+              <div className="d-flex bg-primary rounded-circle justify-content-center align-items-center badge mobile-hidden">
+                <span className="text">2</span>
+              </div>
+            </div>
+            <div>
+              <p className="text-content text-center">Thiết kế UI/UX</p>
+            </div>
+          </div>
+          <div className="col-1 mobile-hidden">{View_2}</div>
+          <div className="col-1">
+            <div className="bg-white rounded-circle mb-3 d-flex justify-content-center align-items-center p-3 position-relative">
+              <img src="/images/workflow-step-03.png" className="img-fluid w-100 h-100"/>
+              <div className="d-flex bg-primary rounded-circle justify-content-center align-items-center badge mobile-hidden">
+                <span className="text">3</span>
+              </div>
+            </div>
+            <div>
+              <p className="text-content text-center">Demo thiết kế</p>
+            </div>
+          </div>
+          <div className="col-1 mobile-hidden">{View_3}</div> 
+          <div className="col-1">
+            <div className="bg-white rounded-circle mb-3 d-flex justify-content-center align-items-center p-3 position-relative">
+              <img src="/images/workflow-step-04.png" className="img-fluid w-100 h-100"/>
+              <div className="d-flex bg-primary rounded-circle justify-content-center align-items-center badge mobile-hidden">
+                <span className="text">4</span>
+              </div>
+            </div>
+            <div>
+              <p className="text-content text-center">Lập trình</p>
+            </div>
+          </div>
+          <div className="col-1 mobile-hidden">{View_4}</div> 
+          <div className="col-1">
+            <div className="bg-white rounded-circle mb-3 d-flex justify-content-center align-items-center p-3 position-relative">
+              <img src="/images/workflow-step-05.png" className="img-fluid w-100 h-100"/>
+              <div className="d-flex bg-primary rounded-circle justify-content-center align-items-center badge mobile-hidden">
+                <span className="text">5</span>
+              </div>
+            </div>
+            <div>
+              <p className="text-content text-center">Bàn giao sản phẩm</p>
+            </div>
+          </div>
+          <div className="col-1 mobile-hidden">{View_5}</div> 
+          <div className="col-1">
+            <div className="bg-white rounded-circle mb-3 d-flex justify-content-center align-items-center p-3 position-relative">
+              <img src="/images/workflow-step-06.png" className="img-fluid w-100 h-100"/>
+              <div className="d-flex bg-primary rounded-circle justify-content-center align-items-center badge mobile-hidden">
+                <span className="text">6</span>
+              </div>
+            </div>
+            <div>
+              <p className="text-content text-center">Bảo hành/Bảo trì</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,59 @@
+"use client"
+import { useState } from "react";
+
+const Header = () => {
+  const [menu, setMenu] = useState([
+    { name: "Trang chủ", link: "#", active: true },
+    { name: "Dịch vụ", link: "#" },
+    { name: "Tin tức", link: "#" },
+    { name: "Giới thiệu", link: "#" },
+    { name: "FAQs", link: "#" },
+  ])
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className="container-fluid">
+      <div className="d-flex container-fluid bg-primary pt-3 pb-3 justify-content-center mobile-hidden">
+        <div className="d-flex container">
+          <div className="w-auto text-white me-auto">
+            <p>
+              <i className="bi bi-telephone"></i> Số điện thoại: 0973023736 |{" "}
+              <i className="bi bi-envelope-check"></i> Email:
+              smartcodesolutions@gmail.com
+            </p>
+          </div>
+          <div className="w-auto text-white ms-auto">
+            <i className="bi bi-facebook me-3"></i>
+            <i className="bi bi-github"></i>
+          </div>
+        </div>
+      </div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src="/images/trans-logo.png" />
+          </a>
+          <button className="navbar-toggler" type="button" onClick={setOpen.bind(null, !open)}>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className={`collapse navbar-collapse ${open ? "show" : ""}`}>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {menu.map((item, index) => (
+                <li className="nav-item" key={index}>
+                  <a className={`nav-link ${item.active ? "active" : ""}`} href={item.link}>
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <button className="btn btn-primary">
+              Liên hệ
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
