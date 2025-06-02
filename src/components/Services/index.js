c2daa315d2f8baaaab0a066d69f92d097bbb867b
Nguyen Trong Nghia (FPL CT) <nghiant96@fpt.edu.vn>
2025-06-02 23:02:33 +0700

    remove log

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -7,31 +7,42 @@ const Services = () => {
   const [activeTab, setActiveTab] = useState(0);
   const [tabs, setTabs] = useState(data);
 
-  console.log(tabs);
-
   return (
     <div className="container-fluid bg-light">
-      <section className="d-flex flex-column container pt-3 pb-3 align-items-center flex-wrap justify-content-center">
-        <div className="col-sm-12 col-lg-6 mb-lg-3 mb-xl-3">
-          <h1 className="text-center">Dịch vụ</h1>
-          <p className="text-center">
-            Tham khảo bảng dịch vụ của chúng tôi để tìm hiểu các gói dịch vụ đa
-            dạng, từ cơ bản đến nâng cao, phù hợp với mọi nhu cầu và ngân sách.
-          </p>
+      <section className="container py-5">
+        <div className="row justify-content-center">
+          <div className="col-sm-12 col-lg-6 mb-3">
+            <h2 className="text-center">Dịch vụ</h2>
+            <p className="text-center">
+              Tham khảo bảng dịch vụ của chúng tôi để tìm hiểu các gói dịch vụ
+              đa dạng, từ cơ bản đến nâng cao, phù hợp với mọi nhu cầu và ngân
+              sách.
+            </p>
+          </div>
         </div>
 
-        <div className="d-flex col-sm-12 col-lg-8 mb-lg-3 mb-xl-3 bg-white rounded rounded-5 p-1 justify-content-between flex-wrap tab shadow">
-          {tabs.map((tab, index) => (
-            <div
-              key={index}
-              className={`col-auto ${
-                activeTab == index ? `active` : ``
-              } rounded rounded-5 p-2 bg-animation cursor-pointer`}
-              onClick={setActiveTab.bind(null, index)}
-            >
-              <p className="text-center">{tab?.name}</p>
+        <div className="row justify-content-center">
+          <div className="col-sm-12 col-lg-10 mb-lg-3 mb-xl-3 tab">
+            <div className="rounded rounded-5 p-2 shadow">
+              <div className="row justify-content-center">
+                {tabs.map((tab, index) => (
+                  <div
+                    key={index}
+                    className={`col-xs-12 col-md-6 col-xl-3`}
+                    onClick={setActiveTab.bind(null, index)}
+                  >
+                    <div
+                      className={`${
+                        activeTab == index ? `active` : ``
+                      } rounded rounded-5 bg-animation cursor-pointer p-1`}
+                    >
+                      <p className="text-center">{tab?.name}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-          ))}
+          </div>
         </div>
 
         {tabs.map((tab, index) => (
@@ -39,34 +50,37 @@ const Services = () => {
             key={index}
             className={`${
               index == activeTab ? `d-flex` : `d-none`
-            } flex-row container-fluid pt-3 pb-3 flex-wrap justify-content-between`}
+            } container-fluid py-3`}
           >
-            {tab.packages.map((packageItem, packageIndex) => (
-              <div
-                key={packageIndex}
-                className="col-sm-10 col-lg-3 shadow rounded rounded-3 p-2 mb-3"
-                style={{ width: "22%" }}
-              >
-                <div className="d-flex align-items-center justify-content-between">
-                  <p>{packageItem?.title}</p>
-                  <button className="btn btn-primary">Liên hệ</button>
-                </div>
-                <hr />
-                {packageItem?.contents.map((content, contentIndex) => (
-                  <div
-                    key={contentIndex}
-                    className="d-flex align-items-center gap-2"
-                  >
-                    {content?.check ? (
-                      <i className="bi bi-check-lg text-success"></i>
-                    ) : (
-                      <i className="bi bi-x-lg text-danger"></i>
-                    )}
-                    <p>{content.title}</p>
+            <div className="row">
+              {tab.packages.map((packageItem, packageIndex) => (
+                <div
+                  key={packageIndex}
+                  className="col-xs-12 col-sm-6 col-xl-3 mb-4"
+                >
+                  <div className="shadow rounded rounded-3 p-4">
+                    <div className="d-flex align-items-center justify-content-between">
+                      <p>{packageItem?.title}</p>
+                      <button className="btn btn-primary">Liên hệ</button>
+                    </div>
+                    <hr />
+                    {packageItem?.contents.map((content, contentIndex) => (
+                      <div
+                        key={contentIndex}
+                        className="d-flex align-items-center gap-2"
+                      >
+                        {content?.check ? (
+                          <i className="bi bi-check-lg text-success"></i>
+                        ) : (
+                          <i className="bi bi-x-lg text-danger"></i>
+                        )}
+                        <p>{content.title}</p>
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
-            ))}
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </section>
